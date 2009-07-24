00b47f822f6a051b227db290f56f17378e222dd6
postmodern <postmodern.mod3@gmail.com>
2009-07-23 23:18:09 -0700

    Made the benchmark.rb script executable.

--- scripts/benchmark.rb
+++ scripts/benchmark.rb
@@ -1,11 +1,14 @@
+#!/usr/bin/env ruby
 $LOAD_PATH.unshift(File.expand_path(File.join(File.dirname(__FILE__),'..','lib')))
 
 require 'wordlist/builder'
 require 'benchmark'
 
+path = File.join(Dir.pwd,'shakespeare_wordlist.txt')
+
 Benchmark.bm do |bm|
   bm.report('build:') do
-    Wordlist::Builder.build('shakespeare_wordlist.txt') do |wordlist|
+    Wordlist::Builder.build(path) do |wordlist|
       wordlist.parse_file('/home/hal/shaks12.txt')
     end
   end
