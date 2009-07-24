75ce52bc3b8e024b7df3495151833ba9cede607f
postmodern <postmodern.mod3@gmail.com>
2009-07-23 21:47:35 -0700

    Added helper constants for the text/ files.

---
+++ spec/helpers/text.rb
@@ -0,0 +1,4 @@
+TEXT_DIR = File.expand_path(File.join(File.dirname(__FILE__),'..','text'))
+
+SAMPLE_TEXT = File.join(TEXT_DIR,'sample.txt')
+PREVIOUS_WORDLIST = File.join(TEXT_DIR,'previous_wordlist.txt')
