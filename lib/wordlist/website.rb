54514f4809d5d2837d294a8ddb2e5e112a6d131a
postmodern <postmodern.mod3@gmail.com>
2009-08-18 15:27:38 -0700

    Refactored Wordlist::Website to inherit from Builder.
    * Override the build! method to spider the website and parse inner_text.
    * Added an attr_accessor for @host.

--- lib/wordlist/website.rb
+++ lib/wordlist/website.rb
@@ -20,29 +20,34 @@
 #++
 #
 
-require 'wordlist/source'
+require 'wordlist/builder'
 
 require 'spidr'
 
 module Wordlist
-  class Website < Source
+  class Website < Builder
 
-    def initialize(host,options={})
-      @host = host
+    # Host to spider
+    attr_accessor :host
 
-      super(options)
+    def initialize(path,options={},&block)
+      @host = options[:host]
+
+      super(path,&block)
     end
 
-    def each_word(&block)
-      Spidr.host(host) do |spidr|
+    def build!(&block)
+      Spidr.host(@host) do |spidr|
         spidr.every_page do |page|
           if page.html?
             page.doc.search('h1|h2|h3|h4|h5|p|span').each do |element|
-              element.inner_text.split.each(&block)
+              parse(element.inner_text)
             end
           end
         end
       end
+
+      super(&block)
     end
 
   end
