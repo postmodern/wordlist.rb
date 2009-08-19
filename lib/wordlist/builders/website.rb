c1bcfede5dcc49fb1f6de6d10f880256b6704aa3
postmodern <postmodern.mod3@gmail.com>
2009-08-19 13:50:35 -0700

    Moved Wordlist::Website into the Wordlist::Builders namespace.

--- lib/wordlist/builders/website.rb
+++ lib/wordlist/builders/website.rb
@@ -25,30 +25,32 @@ require 'wordlist/builder'
 require 'spidr'
 
 module Wordlist
-  class Website < Builder
+  module Builders
+    class Website < Builder
 
-    # Host to spider
-    attr_accessor :host
+      # Host to spider
+      attr_accessor :host
 
-    def initialize(path,options={},&block)
-      @host = options[:host]
+      def initialize(path,options={},&block)
+        @host = options[:host]
 
-      super(path,&block)
-    end
+        super(path,&block)
+      end
 
-    def build!(&block)
-      Spidr.host(@host) do |spidr|
-        spidr.every_page do |page|
-          if page.html?
-            page.doc.search('h1|h2|h3|h4|h5|p|span').each do |element|
-              parse(element.inner_text)
+      def build!(&block)
+        Spidr.host(@host) do |spidr|
+          spidr.every_page do |page|
+            if page.html?
+              page.doc.search('h1|h2|h3|h4|h5|p|span').each do |element|
+                parse(element.inner_text)
+              end
             end
           end
         end
+
+        super(&block)
       end
 
-      super(&block)
     end
-
   end
 end
