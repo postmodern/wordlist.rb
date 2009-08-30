a78ba1e8ded0d303d359ac44b5c39616b99d3c1e
postmodern <postmodern.mod3@gmail.com>
2009-08-29 19:29:56 -0700

    Added documentation to Wordlist::Formats::FlatFile.

--- lib/wordlist/formats/flat_file.rb
+++ lib/wordlist/formats/flat_file.rb
@@ -7,12 +7,24 @@ module Wordlist
       # The path to the flat-file
       attr_accessor :path
 
+      #
+      # Creates a new FlatFile list with the specified _path_ and given
+      # _options_.
+      #
       def initialize(path,options={})
         @path = path
 
         super(options)
       end
 
+      #
+      # Enumerates through every word in the flat-file, passing each
+      # word to the given _block_.
+      #
+      #   flat_file.each_word do |word|
+      #     puts word
+      #   end
+      #
       def each_word(&block)
         File.open(@path) do |file|
           file.each_line do |line|
