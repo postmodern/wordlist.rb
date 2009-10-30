53915c09895a4fd41fcef96d01603cef83d2e02b
postmodern <postmodern.mod3@gmail.com>
2009-10-30 02:25:44 -0700

    Added specs for Builder#word_combinations.

--- spec/builder_spec.rb
+++ spec/builder_spec.rb
@@ -57,4 +57,71 @@ describe Builder do
       @builder.word_queue.should == ['cat', 'log']
     end
   end
+
+  describe "word combinations" do
+    before(:all) do
+      @path = wordlist_tempfile
+    end
+
+    it "should yield only one word when max_words is set to 1" do
+      builder = Builder.new(@path)
+      builder.enqueue('dog')
+
+      builder.word_combinations do |words|
+        words.should == 'dog'
+      end
+    end
+
+    it "should include the last seen word in every combination" do
+      builder = Builder.new(@path, :max_words => 2)
+      builder.enqueue('dog')
+      builder.enqueue('cat')
+      builder.enqueue('dat')
+
+      builder.word_combinations do |words|
+        words.split(' ').include?('dat').should == true
+      end
+    end
+
+    it "should include a minimum number of words" do
+      builder = Builder.new(@path, :min_words => 2, :max_words => 3)
+      builder.enqueue('dog')
+      builder.enqueue('cat')
+      builder.enqueue('dat')
+
+      builder.word_combinations do |words|
+        words.split(' ').length.should >= 2
+      end
+    end
+
+    it "should not include more than a maximum number of words" do
+      builder = Builder.new(@path, :max_words => 2)
+      builder.enqueue('dog')
+      builder.enqueue('cat')
+      builder.enqueue('dat')
+
+      builder.word_combinations do |words|
+        words.split(' ').length.should_not > 2
+      end
+    end
+
+    it "should preserve the order words were seen in" do
+      builder = Builder.new(@path, :max_words => 3)
+      builder.enqueue('dog')
+      builder.enqueue('cat')
+      builder.enqueue('dat')
+
+      combinations = []
+
+      builder.word_combinations do |words|
+        combinations << words
+      end
+
+      combinations.should == [
+        ['dat'],
+        ['cat dat'],
+        ['dog cat dat']
+      ]
+    end
+  end
 end
