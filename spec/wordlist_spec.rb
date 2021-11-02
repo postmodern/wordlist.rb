f1fc47ffa0d0cf4da8850b0ed404ccf2a90bc1b5
Postmodern <postmodern.mod3@gmail.com>
2021-11-01 18:37:44 -0700

    Added specs for Wordlist.open and Wordlist.build.

--- spec/wordlist_spec.rb
+++ spec/wordlist_spec.rb
@@ -1,8 +1,44 @@
 require 'spec_helper'
-require 'wordlist/version'
+require 'wordlist'
 
 describe Wordlist do
   it "should have a VERSION constant" do
     expect(Wordlist.const_defined?('VERSION')).to be(true)
+  end
+
+  let(:fixtures_dir) { ::File.join(__dir__,'fixtures') }
+
+  describe ".open" do
+    let(:path) { ::File.join(fixtures_dir,'wordlist.txt') }
+
+    it "must return a Wordlist::File object" do
+      expect(subject.open(path)).to be_kind_of(Wordlist::File)
+    end
+
+    context "when given a block" do
+      it "must yield the Wordlist::File object" do
+        expect { |b|
+          subject.open(path,&b)
+        }.to yield_with_args(Wordlist::File)
+      end
+    end
+  end
+
+  describe ".build" do
+    let(:path) { ::File.join(fixtures_dir,'new_wordlist.txt') }
+
+    it "must return a Wordlist::Builder object" do
+      expect(subject.build(path)).to be_kind_of(Wordlist::Builder)
+    end
+
+    context "when given a block" do
+      it "must yield the Wordlist::Builder object" do
+        expect { |b|
+          subject.build(path,&b)
+        }.to yield_with_args(Wordlist::Builder)
+      end
+    end
+
+    after { ::FileUtils.rm_f(path) }
   end
 end
